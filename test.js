ce215ab013af724942ad7131b573be63facd9045
bacloud14.ubu <bacloud14@gmail.com>
2022-06-24 13:45:30 +0200

    made an ava test

--- test.js
+++ test.js
@@ -1,19 +1,39 @@
-const NLPEscape = require("../index").NLPEscape;
+const test = require("ava");
+const NLPEscape = require("./index").NLPEscape;
 
 let text =
   "this is a <b>big</b> data text. But what is big data? <a>read more</a>." +
   "Then <i>vＥⓡ𝔂 𝔽𝕌Ňℕｙ</i> ţ乇𝕏𝓣 and finally ssn:987477475 ssn:987 47 7475 wow this is working";
-console.log(text);
+console.log(`Original input:\n ${text}\n`);
 
-const lib = new NLPEscape(["<a>", "<b>", "</b>", "</a>", "<i>", "</i>"]);
-const clean = lib.escape(text);
-console.log(clean);
+test("Escape keeps natural language intact", (t) => {
+  const lib = new NLPEscape(["<a>", "<b>", "</b>", "</a>", "<i>", "</i>"]);
+  const clean = lib.escape(text);
+  const strippedClean = clean.split(/[\0]+/);
+  const strippedHTML = text.split(/<[^>]*>?/);
+  strippedHTML.forEach((str, idx) => {
+    t.is(str, strippedClean[idx]);
+  });
+});
 
-text = lib.unescape(clean);
-console.log(text);
-// State is fine ?
-text = lib.unescape(clean);
-console.log(text);
+test("Unescape reverts escape", async (t) => {
+  const lib = new NLPEscape(["<a>", "<b>", "</b>", "</a>", "<i>", "</i>"]);
+  const clean = lib.escape(text);
+  let original = lib.unescape(clean);
+  t.is(text, original);
+});
+
+test("Real world example. Please use your own pipeline and compare results", async (t) => {
+  const lib = new NLPEscape(["<a>", "<b>", "</b>", "</a>", "<i>", "</i>"]);
+  const clean = lib.escape(text);
+  const result = new stringTransformer(clean)
+    .decancer()
+    .cleanSensitive()
+    .valueOf();
+  let original = lib.unescape(result);
+  console.log(`\nRecovered results:\n ${original}`);
+  t.pass();
+});
 
 // Real world example
 // This is one of your pipeline stages (external library)
@@ -22,13 +42,14 @@ const decancer = require("decancer");
 // List of things to censor
 // Credit: http://www.richardsramblings.com/regex/credit-card-numbers/ && https://codepen.io/gpeu/pen/eEdvmO
 const reb = {
-  discover: /\b6(?:011|5\d{2}|22[19]|4[56789]\d{1})[ -./\\]?\d{4}[ -./\\]?\d{4}[ -./\\]?\d{4}\b/g,
+  discover:
+    /\b6(?:011|5\d{2}|22[19]|4[56789]\d{1})[ -./\\]?\d{4}[ -./\\]?\d{4}[ -./\\]?\d{4}\b/g,
   jcb: /\b(?:2131|1800|35\d[28-89])[ -./\\]?\d{4}[ -./\\]?\d{4}[ -./\\]?\d{4}\b/g,
   ssn: /\b\d{3}[ -./\\]?\d{2}[ -./\\]?\d{4}\b/g,
-}
+};
 const rew = {
   phone: /\b(?:(?:\(\d{3}\)?)|(?:\d{3}))[ -./\\]?\d{3}[ -./\\]?\d{4}\b/g,
-}
+};
 function cleanSensitive(blob, maxLen) {
   if (maxLen === 0) return "";
   if (maxLen < 9) return blob;
@@ -75,10 +96,3 @@ function stringTransformer(s) {
     return internal;
   };
 }
-// Pipeline execution
-const res = new stringTransformer(clean).decancer().cleanSensitive().valueOf()
-console.log(res)
-// Recover tags
-const final = lib.unescape(res);
-console.log(final)
-
